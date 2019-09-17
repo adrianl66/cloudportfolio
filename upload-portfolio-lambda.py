34087c3f625d09e92728217d7dd1553546295857
adrianl66 <adrian_eh.lim@yahoo.com>
2019-09-17 20:33:55 +0800

    added upload-portfolio-lambda.py

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,7 +8,7 @@ s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 portfolio_bucket = s3.Bucket('www.a3isolutions.com')
 build_bucket = s3.Bucket('a3isolutionsbuild')
 
-portfolio_zip = StringIO.StringIO()
+portfolio_zip = StringIO()
 build_bucket.download_fileobj('a3isolutionsBuild.zip', portfolio_zip)
 
 with zipfile.Zipfile(portfolio.zip) as myzip:
