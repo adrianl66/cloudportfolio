7e78bc8a2eeb1c4392972ce0f907c07cef5e0bdc
adrianl66 <adrian_eh.lim@yahoo.com>
2019-09-18 15:19:14 +0800

    modifie4d for execution in aws lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,17 +6,26 @@ import io
 import json
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    portfolio_bucket = s3.Bucket('www.a3isolutions.com')
-    build_bucket = s3.Bucket('a3isolutionsbuild')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:ap-southeast-1:937336577693:deployPortfolioTopic')
 
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('a3isolutionsBuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        portfolio_bucket = s3.Bucket('www.a3isolutions.com')
+        build_bucket = s3.Bucket('a3isolutionsbuild')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('a3isolutionsBuild.zip', portfolio_zip)
 
-    return 'Completed copying files'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print ('Completed copying files')
+        topic.publish(Subject='Deploy Successful', Message='www.a3isolutions.com site deployed succesfully!')
+
+    except:
+        topic.publish(Subject='Deploy Failed', Message='www.a3isolutions.com site was NOT deployed succesfully')
+        raise
