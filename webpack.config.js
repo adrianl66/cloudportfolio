4b60244fb74d3c40806ba500ef533da0daa41cc1
adrianl66 <adrian_eh.lim@yahoo.com>
2019-09-25 18:23:30 +0800

    revised

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const webpack = require('webpack');
 const path = require('path');
 module.exports = {
+  mode: 'production',
   entry: './js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -7,7 +9,7 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.(js | jsx)$/,
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
